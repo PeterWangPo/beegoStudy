67ccb79d41af09c6595f6f42530507bcad193284
wangyibo5843 <wangyibo5843@gmx.com>
2017-08-25 14:36:04 +0800

    登陆设置session修改

--- controllers/base.go
+++ controllers/base.go
@@ -3,12 +3,27 @@ package controllers
 import (
 	"fmt"
 	"github.com/astaxie/beego"
+	"strconv"
+	"strings"
 )
 
 type BaseController struct {
 	beego.Controller
+	IsLogin  bool
+	UserId   int64
+	Username string
 }
 
 func (this *BaseController) Prepare() {
+	sessionStr := this.GetSession("userInfo")
+	fmt.Println(sessionStr)
+	if sessionStr != nil {
+		sessStr := strings.Split((sessionStr).(string), "|")
+		this.IsLogin = true
+		this.UserId, _ = strconv.ParseInt(sessStr[0], 10, 64)
+		this.Username = sessStr[1]
+	}
+	fmt.Println(this.UserId)
+	fmt.Println(this.Username)
 	fmt.Println("base prepare done...")
 }
